d88c803e341f07fa073c0b50688a4f293a2573ef
Spencer Watson <wattyrev@gmail.com>
2021-12-20 10:18:25 -0800

    Try connecting to dynamo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 /* eslint-disable zillow/import/no-extraneous-dependencies, no-console */
 const express = require('express');
-const mysql = require('mysql');
+const AWS = require('aws-sdk');
 
 const app = express();
-
-// const sqlConnection = mysql.createConnection({
-//     user: process.env.SQL_USER,
-//     database: process.env.SQL_DATABASE,
-//     password: process.env.SQL_PASSWORD,
-//     socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
-// });
-// sqlConnection.connect();
 
 // Set headers for all APIs
 app.use((request, response, next) => {
@@ -31,5 +23,17 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/', (request, response) => {
-    response.send('test 8');
+    const dynamoClient = new AWS.DynamoDB.DocumentClient();
+    dynamoClient.scan({ TableName: 'kitepaint-users-beta', (error, data) => {
+        if (error) {
+            response.status(500).send({
+                message: error,
+            });
+            return;
+        }
+        const { Items } = data;
+        response.send({
+            users: Items
+        });
+    }});
 });
