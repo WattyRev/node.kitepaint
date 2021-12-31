154c6ed97699c641046047cf93f75cd60bdccbd3
Spencer Watson <wattyrev@gmail.com>
2021-12-30 21:47:38 -0800

    try 443 again

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use((request, response, next) => {
 app.use(express.json());
 
 // Open the server
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 443;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
